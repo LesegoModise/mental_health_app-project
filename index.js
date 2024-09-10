ae079cb6dbb41a89ca774d54150b705a60a564da
Lesego Modise <lesegonhlanhla18@gmail.com>
2024-09-10 10:21:48 +0200

    Started styling and added a few changes on the welcome screen

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // This file should set up the server, middleware and route handling
 // This will be an entry point 
 // We should set up routes for various functionalities like authentication, mood entry, journaling and reporting
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'))
+app.use(express.json())
+
+const PORT = process.env.PORT || 4011;
+app.listen(PORT, function () {
+    console.log(`Server started http://localhost:${PORT}`);
+})
