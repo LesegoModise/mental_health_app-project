06d7ee3c2316756747400fdd896b354cbdab6772
orifha <orifhamatshidze@gmail.com>
2024-09-16 11:28:47 +0200

    initial commit: added files

--- public/report.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('dashboard', () => ({
-
-        navigateToRecommendation() {
-            window.location.href = './recommendation.html';
-        }
-
-    }))
-})
