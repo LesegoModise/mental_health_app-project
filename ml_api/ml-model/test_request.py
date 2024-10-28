780578b5e90f3efe40254356a875595f197124c4
Lesego Modise <lesegonhlanhla18@gmail.com>
2024-10-28 12:29:59 +0200

    I altered the port number for the ML so that our ML on render works successfully

--- ml_api/ml-model/test_request.py
+++ ml_api/ml-model/test_request.py
@@ -1,7 +1,21 @@
-import requests
+# import requests
 
-url = 'http://127.0.0.1:5000/predict'
-input_data = {'statement': 'I feel happy and energetic today.'}
+# url = 'http://127.0.0.1:5000/predict'
+# input_data = {'statement': 'I feel happy and energetic today.'}
 
-response = requests.post(url, json=input_data)
-print(response.json())  # This will print the prediction from the model
+# response = requests.post(url, json=input_data)
+# print(response.json())  # This will print the prediction from the model
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
