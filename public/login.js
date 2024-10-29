3a201679ad74329c5cf899966e8d34d091eee302
Lesego Modise <lesegonhlanhla18@gmail.com>
2024-10-29 16:30:59 +0200

    I added the admin page for the predictions

--- public/login.js
+++ public/login.js
@@ -2,22 +2,40 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('login', () => ({
         username: '',
         password: '',
+        role: 'user',
         rememberMe: false,
         errors: {},
 
         async submit(event) {
             event.preventDefault();
 
-            const correctUsername = 'Eric Nkosi';
-            const correctPassword = 'EricNkosi91';
+            const userCredentials = {
+                username: 'Eric Nkosi',
+                password: 'EricNkosi91'
+            };
 
-            if (this.username === correctUsername && this.password === correctPassword) {
+            const adminCredentials = {
+                username: 'Cynthia Genaro',
+                password: 'CynthiaGenaroAdmin!'
+            };
+
+
+            if (
+                (this.role === 'user' &&
+                    this.username === userCredentials.username &&
+                    this.password === userCredentials.password) ||
+                (this.role === 'admin' &&
+                    this.username === adminCredentials.username &&
+                    this.password === adminCredentials.password)
+            ) {
                 localStorage.setItem('isLoggedIn', 'true');
-                location.href = './mood_selection.html';
+                localStorage.setItem('userRole', this.role);
+
+                location.href = this.role === 'admin' ? './admin_dashboard.html' : './mood_selection.html';
             } else {
                 this.errors = {
-                    username: 'Invalid username or password',
-                    password: 'Invalid username or password'
+                    username: 'Invalid username',
+                    password: 'Invalid password'
                 };
             }
         },
